4cf0bc74b5ff63a31959cef0f8c2f8b589bca7ba
Patrick Kubiak <Patrick Kubiak>
2017-01-17 00:50:37 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="theme-color" content="#E64A19">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antuple</title>
+    <link rel="author" href="https://antuple.net">
+    <link rel="stylesheet" href="https://bootswatch.com/paper/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/application.css">
+</head>
+
+<body>
+    <div class="jumbotron">
+        <div class="container">
+            <h1>Global Perspective</h1>
+            <p>
+                <a href="/" target="_blank" class="btn btn-danger btn-lg"><i class="fa fa-link"></i> Antuple</a>
+            </p>
+        </div>
+    </div>
+    <div class="container">
+				
+		<div class="panel panel-danger">
+			<div class="panel-heading">
+				<h3 class="panel-title">The Man Without a Face: The Unlikely Rise of Vladimir Putin</h3>
+			</div>
+			<div class="panel-body">
+				<img src="assets/banner.jpg" class="img-responsive img-rounded" style="margin: 0 auto 10px auto;">
+				An understanding of the global perspective present in The Man Without a Face: The Unlikely Rise of Vladimir Putin by Masha Gessen.
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-md-6">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title">Excerpt From Text</h3>
+					</div>
+					<div class="panel-body">
+						<a href="assets/book.pdf" target="_blank" class="btn btn-danger btn-lg"><i class="fa fa-book fa-5x"></i></a><br>
+						A short passage from the book.
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-md-6">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title">Quiz</h3>
+					</div>
+					<div class="panel-body">
+						<a href="https://docs.google.com/forms/d/1FXJ42BUr8Q8EzJ1Da3Q8AD7jBTi-iqII_oa2iyl2smM/viewform" target="_blank" class="btn btn-danger btn-lg"><i class="fa fa-pencil fa-5x"></i></a><br>
+						Take the quiz to test your understanding.
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+		
+		<div class="panel panel-success">
+			<div class="panel-heading">
+				<h3 class="panel-title">Quick Facts</h3>
+			</div>
+			<div class="panel-body">
+				<ul class="list-group">
+					<li class="list-group-item">
+						Vladimir Putin was the president of Russia from 2000 to 2008, and reelected as President in 2012.
+					</li>
+					<li class="list-group-item">
+						Kremlin is the official residence of the President of the Russian Federation located in Moscow, Russia. Kremlin is the office of the government of the Russian Federation, similar to the White House which is the executive office of the president of the United States.
+					</li>
+					<li class="list-group-item">
+						Putin was an officer in the KGB for 16 years.
+					</li>
+					<li class="list-group-item">
+						Gazprom is a state owned gas monopoly.
+					</li>
+				</ul>
+			</div>
+		</div>
+		
+		<div class="page-header page-banner"></div>
+		<div id="footer">
+			<ul class="list-inline pull-right">
+				<li><a target="_blank" href="https://github.com/patkub"><i class="fa fa-github"></i></a></li>
+			</ul>
+			<small>&copy; <?php echo date("Y") ?> Patrick Kubiak</small>
+		</div>
+    </div>
+</body>
+
+<!-- jQuery -->
+<script src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
+
+<!-- Bootstrap Latest compiled and minified JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+</html>
